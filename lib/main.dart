811b6f84b383a239635e472fa5fed98e751ec2be
Lucas Cristiano <lucaslopes198@gmail.com>
2022-05-11 23:35:44 -0300

    first commit this project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter!!!'),
+      ),
+    );
+  }
+}
