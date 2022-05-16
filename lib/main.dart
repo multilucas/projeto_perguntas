4327f7eefaecbcc520cb4cf95c5a1560c37197ca
multilucas <lucaslopes198@gmail.com>
2022-05-16 11:46:05 -0300

    Até aula 37

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // ignore: unused_import
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 main() {
@@ -6,13 +8,38 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       ),
     );
   }
